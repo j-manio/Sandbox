7bdc1508f6c6e89cbd8d1354a153a731326df579
j-manio <john..manio@my.jcu.edu.au>
2023-09-22 03:01:04 +1000

    Add a pseudocode

---
+++ password_stars.py
@@ -0,0 +1,13 @@
+""" Password Stars """
+
+"""
+Pseudocode
+Program that ask user for a password, with error checking to repeat if the password doesn't meet
+the minimum length set by the variable.
+
+get user password
+error check if the input doesn't match the minimum length
+
+the program should print "*" same length as the password
+
+"""
